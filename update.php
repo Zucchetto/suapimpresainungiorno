f8e99b03e2ad8b128234b97a15d35d012e40c788
Francesco Piero Paolicelli <piersoft@me.com>
2016-06-30 15:29:42 +0200

    inserite variabili per il percorso file

--- update.php
+++ update.php
@@ -1,12 +1,14 @@
 <?php
 $city="matera"; //inserire città in minuscolo
 $id="F052"; //inserire codice di impresainungiorno. di solito è il CAP
+$path="/usr/www/piersoft/suapmatera/db/falairnew.txt";
+$pathcsv="/usr/www/piersoft/suapmatera/db/export.csv";
 exec('curl -v -c cookies.txt "https://www.impresainungiorno.gov.it" ');
 
-exec('curl -v -b cookies.txt -L "https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Host: www.impresainungiorno.gov.it" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" -H "Accept: text/csv,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Language: it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4,cs;q=0.2" -H "Upgrade-Insecure-Requests: 1" -H "Accept-Encoding: gzip, deflate, sdch, br" --compressed -H "Referer: https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Connection: keep-alive" > /usr/www/piersoft/suapmatera/db/falairnew.txt');
+exec('curl -v -b cookies.txt -L "https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Host: www.impresainungiorno.gov.it" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" -H "Accept: text/csv,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Language: it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4,cs;q=0.2" -H "Upgrade-Insecure-Requests: 1" -H "Accept-Encoding: gzip, deflate, sdch, br" --compressed -H "Referer: https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Connection: keep-alive" > '.$path.'');
 
-	$html = file_get_contents("/usr/www/piersoft/suapmatera/db/falairnew.txt");
-    $html=str_replace(",","",$html);
+	$html = file_get_contents($path);
+  $html=str_replace(",","",$html);
   $html=str_replace("p_auth=","<prova>",$html);
   $html=str_replace("&p_p_id=","<prova>",$html);
 
@@ -16,7 +18,7 @@ function getInnerSubstring($string,$delim){
 }
 $auth=getInnerSubstring($html,'<prova>');
 $data="p_auth=".$auth."&p_p_id=3_WAR_servizi_attiviportlet&p_p_lifecycle=1&p_p_state=normal&p_p_mode=view&p_p_col_id=column-2&p_p_col_pos=1&p_p_col_count=3&_3_WAR_servizi_attiviportlet_cciaa=MT&_3_WAR_servizi_attiviportlet_javax.portlet.action=exportCSV";
-exec('curl -v -b cookies.txt -L "https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Host: www.impresainungiorno.gov.it" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" -H "Accept: text/csv,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Language: it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4,cs;q=0.2" -H "Upgrade-Insecure-Requests: 1" -H "Accept-Encoding: gzip, deflate, sdch, br" --compressed -H "Referer: https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Connection: keep-alive" --data "'.$data.'" > /usr/www/piersoft/suapmatera/db/export.csv');
+exec('curl -v -b cookies.txt -L "https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Host: www.impresainungiorno.gov.it" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" -H "Accept: text/csv,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Accept-Language: it-IT,it;q=0.8,en-US;q=0.6,en;q=0.4,cs;q=0.2" -H "Upgrade-Insecure-Requests: 1" -H "Accept-Encoding: gzip, deflate, sdch, br" --compressed -H "Referer: https://www.impresainungiorno.gov.it/web/'.$city.'/comune/pratiche-presentate/t/'.$id.'" -H "Connection: keep-alive" --data "'.$data.'" > '.$pathcsv.'');
 
 
 echo "finito";
